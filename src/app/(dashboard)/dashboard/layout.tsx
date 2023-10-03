78081eb93328917a3bad530f124e9d5c0421d00f
VanshikaP <vanshikapundir92@gmail.com>
2023-10-03 11:34:24 -0700

    final commit

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,27 +1,27 @@
-import { Icons } from "@/components/icons";
+import { Icon, Icons } from "@/components/Icons";
+import SignOutButton from "@/components/SignOutButton";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
-import { Icon } from "@/components/icons";
-import Image from "next/image";
-import SignOutButton from "@/components/SignOutButton";
 import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOptions";
 import { fetchRedis } from "@/helpers/redis";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
 import SidebarChatList from "@/components/SidebarChatList";
+import MobileChatLayout from "@/components/MobileChatLayout";
+import { SidebarOption } from "@/types/typings";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-interface SidebarOption {
-  id: number;
-  name: string;
-  href: string;
-  Icon: Icon;
-}
+// Done after the video and optional: add page metadata
+export const metadata = {
+  title: "FriendZone | Dashboard",
+  description: "Your dashboard",
+};
 
 const sidebarOptions: SidebarOption[] = [
   {
@@ -32,11 +32,13 @@ const sidebarOptions: SidebarOption[] = [
   },
 ];
 
-const Layout: FC<LayoutProps> = async ({ children }) => {
+const Layout = async ({ children }: LayoutProps) => {
   const session = await getServerSession(authOptions);
-  if (!session) {
-    notFound();
-  }
+  if (!session) notFound();
+
+  const friends = await getFriendsByUserId(session.user.id);
+  console.log("friends", friends);
+
   const unseenRequestCount = (
     (await fetchRedis(
       "smembers",
@@ -44,28 +46,38 @@ const Layout: FC<LayoutProps> = async ({ children }) => {
     )) as User[]
   ).length;
 
-  const friends = await getFriendsByUserId(session.user.id);
-
   return (
     <div className="w-full flex h-screen">
-      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-        <Link className="flex h-16 shrink-0 items-center" href="/dashboard">
-          <Icons.Logo className="h-8 w-auto text-indigo-600"></Icons.Logo>
+      <div className="md:hidden">
+        <MobileChatLayout
+          friends={friends}
+          session={session}
+          sidebarOptions={sidebarOptions}
+          unseenRequestCount={unseenRequestCount}
+        />
+      </div>
+
+      <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+        <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+          <Icons.Logo className="h-8 w-auto text-indigo-600" />
         </Link>
+
         {friends.length > 0 ? (
           <div className="text-xs font-semibold leading-6 text-gray-400">
             Your chats
           </div>
         ) : null}
+
         <nav className="flex flex-1 flex-col">
           <ul role="list" className="flex flex-1 flex-col gap-y-7">
             <li>
-              <SidebarChatList friends={friends} sessionId={session.user.id} />
+              <SidebarChatList sessionId={session.user.id} friends={friends} />
             </li>
             <li>
               <div className="text-xs font-semibold leading-6 text-gray-400">
                 Overview
               </div>
+
               <ul role="list" className="-mx-2 mt-2 space-y-1">
                 {sidebarOptions.map((option) => {
                   const Icon = Icons[option.Icon];
@@ -78,6 +90,7 @@ const Layout: FC<LayoutProps> = async ({ children }) => {
                         <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
                           <Icon className="h-4 w-4" />
                         </span>
+
                         <span className="truncate">{option.name}</span>
                       </Link>
                     </li>
@@ -104,6 +117,7 @@ const Layout: FC<LayoutProps> = async ({ children }) => {
                     alt="Your profile picture"
                   />
                 </div>
+
                 <span className="sr-only">Your profile</span>
                 <div className="flex flex-col">
                   <span aria-hidden="true">{session.user.name}</span>
@@ -112,12 +126,14 @@ const Layout: FC<LayoutProps> = async ({ children }) => {
                   </span>
                 </div>
               </div>
+
               <SignOutButton className="h-full aspect-square" />
             </li>
           </ul>
         </nav>
       </div>
-      <aside className="max-h-screen overflow-y-auto container py-16 md:py-12 w-full">
+
+      <aside className="max-h-screen container py-16 md:py-12 w-full">
         {children}
       </aside>
     </div>
