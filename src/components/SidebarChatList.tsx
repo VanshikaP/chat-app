0a94256ac90ed0ae8f606532e9ab42aaeac00d24
VanshikaP <vanshikapundir92@gmail.com>
2023-10-05 12:15:21 -0700

    bugfix for active chat list

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -73,7 +73,7 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ friends, sessionId }) => {
   }, [pathname]);
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-      {friends.sort().map((friend) => {
+      {activeChats.sort().map((friend) => {
         const unseenMessagesCount = unseenMessages.filter((unseenMessage) => {
           return unseenMessage.senderId === friend.id;
         }).length;
